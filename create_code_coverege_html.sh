8e6cba1afa497fef07a87b393b4f1af652a5e0b4
yaishenka <gagarinovdaniil@gmail.com>
2019-11-26 18:37:20 +0300

    change dir

--- create_code_coverege_html.sh
+++ create_code_coverege_html.sh
@@ -8,5 +8,5 @@ make Test
 ./Test
 cd ..
 
-lcov --directory ./build/CMakeFiles/Test.dir/ --capture --output-file ./code_coverage.info -rc lcov_branch_coverage=1
+lcov --directory /home/travis/build/yaishenka/FormalLanguagesPracticum/build/Test.dir/ --capture --output-file ./code_coverage.info -rc lcov_branch_coverage=1
 genhtml code_coverage.info --branch-coverage --output-directory ./code_coverage_report/
